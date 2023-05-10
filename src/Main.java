d0f95ce47984cd334f349af9f1cb0c79a510a2b1
Marcelo Cesar <marcelocesar9812@gmail.com>
2023-05-10 13:08:01 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		char resp;
+        do  {
+        	System.out.print("Digite a temperatura em Celsius: ");
+		
+		
+        double C = sc.nextDouble();
+        double F = 9.0 * C / 5.0 +32.0;
+        System.out.printf("Equivale em Fahrenheit: %.2f",F);
+        System.out.println();
+        System.out.print("Deseja repetir s/n ?");
+        resp = sc.next().charAt(0);
+        }
+        while(resp!='n');
+        sc.close();
+        
+	}
+        
+}
